2847c0ef4a810f098fc4c5f267fb8ebb5c12f42c
Matthew <mail@plvskiy.ru>
2020-11-09 23:13:32 +0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='regru_cloudapi',
+setup(
+      name='regru_cloudapi',
       version='1.0',
       url='https://github.com/plvskiy/regru_cloudapi',
       author='Matthew Polovskiy',
-      description='Add static script_dir() method to Path',
       packages=find_packages(),
-      python_requires='>=3.6')
+      python_requires='>=3.6'
+)
