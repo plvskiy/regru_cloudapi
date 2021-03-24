329e185ffe6b7a97524d6693e02f21bc18182232
Matthew Polovskiy <mail@plvskiy.ru>
2021-03-24 22:29:07 +0400

    Откат

--- regru_cloudapi/__version__.py
+++
@@ -1,5 +0,0 @@
-__title__ = 'regru_cloudapi'
-__description__ = 'Reg.ru CloudAPI Python library'
-__version__ = '1.2.3'
-__url__ = 'https://github.com/plvskiy/regru_cloudapi'
-__author__ = 'Matthew Polovskiy'
--- setup.py
+++ setup.py
@@ -9,17 +9,12 @@ def read(filename):
         return file.read()
 
 
-about = {}
-with open(os.path.join(here, 'regru_cloudapi', '__version__.py'), 'r') as f:
-    exec(f.read(), about)
-
-
 setup(
-    name=about['__title__'],
-    description=about['__description__'],
-    version=about['__version__'],
-    url=about['__url__'],
-    author=about['__author__'],
+    name='regru_cloudapi',
+    description='Reg.ru CloudAPI Python library',
+    version='1.2.4',
+    url='https://github.com/plvskiy/regru_cloudapi',
+    author='Matthew Polovskiy',
     packages=find_packages(),
     python_requires='>=3.6',
     long_description=read('README.md'),
